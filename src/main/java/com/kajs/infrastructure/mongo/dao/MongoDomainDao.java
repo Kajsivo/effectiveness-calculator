41cf4bf952b0e041c165f67b6c4dbe9f842c1215
Kamil Lukaszewski <kamil.lukaszewski@allegrogroup.com>
2017-02-03 13:16:39 +0100

    Fix the illegal argument exception causes by the repository annotation

--- src/main/java/com/kajs/infrastructure/mongo/dao/MongoDomainDao.java
+++ src/main/java/com/kajs/infrastructure/mongo/dao/MongoDomainDao.java
@@ -2,8 +2,9 @@ package com.kajs.infrastructure.mongo.dao;
 
 import com.kajs.infrastructure.mongo.entity.DomainEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Component
 public interface MongoDomainDao extends MongoRepository<DomainEntity, String>, MongoDomainDaoCustom{
 }
--- src/main/java/com/kajs/infrastructure/mongo/dao/MongoKeeperDao.java
+++ src/main/java/com/kajs/infrastructure/mongo/dao/MongoKeeperDao.java
@@ -2,8 +2,9 @@ package com.kajs.infrastructure.mongo.dao;
 
 import com.kajs.infrastructure.mongo.entity.KeeperEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Component
 public interface MongoKeeperDao extends MongoRepository<KeeperEntity, String>, MongoKeeperDaoCustom {
 }
